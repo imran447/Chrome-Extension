27f237c6bf7ebaf5390a51392279a10725bb13e7
imran447 <40700209+imran447@users.noreply.github.com>
2020-12-20 21:12:17 +0500

    Design update

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  google_bar :boolean=false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  manageGoogleBar=()=>{
+    this.google_bar=!(this.google_bar);
   }
 
 }
--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -6,12 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-  settingManage : boolean = false;
+  settingManage     : boolean = false;
+  sideFeedBar       : boolean = false;
+  notificationManage :boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
   profileSettingMange=()=>{
     this.settingManage=!(this.settingManage);
+  }
+  sideFeedBarManage=()=>{
+    this.sideFeedBar=!(this.sideFeedBar);
+  }
+  notificationOpen=()=>{
+    this.notificationManage=!(this.notificationManage);
   }
 }
