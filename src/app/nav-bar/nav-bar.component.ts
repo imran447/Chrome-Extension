96ffc3266de54ebb43213e07043fd5965624ce6b
imran447 <40700209+imran447@users.noreply.github.com>
2021-01-10 22:45:48 +0500

    backend and login Form

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,12 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   google_bar :boolean=false;
+  chromeTopSite: boolean=false;
   constructor() { }
 
   ngOnInit(): void {
   }
   manageGoogleBar=()=>{
     this.google_bar=!(this.google_bar);
+  }
+  handleChromeTopSites=()=>{
+    this.chromeTopSite=!this.chromeTopSite;
   }
 
 }
